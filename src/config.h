aeb2cffa078e54f4737e37ea5837226f6205b684
JustBKhere <90069354+justBKhere@users.noreply.github.com>
2024-12-31 14:12:50 +0530

    Update config.h

--- src/config.h
+++ src/config.h
@@ -12,8 +12,9 @@ __device__ const int MAX_PATTERNS = 10;
 // exact matches at the beginning of the address, letter ? is wildcard
 
 __device__ static char const *prefixes[] = {
-	"AAAAA",
-	"BBBBB",
+	"VORLD",
+	"Vorld",
+"VRLD,
 };
 
 
